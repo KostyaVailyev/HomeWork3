d510656690a92b2daa1c96ec1ad4ddde3e25d0ad
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-11-30 19:06:52 +0300

    add palindorm

---
+++ palindorm/palindorm.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int pal = 0, num = number;
+while (number > 0)
+{
+    pal = pal * 10 + number % 10; number /= 10;
+}
+Console.WriteLine(num == pal ? $"Число {num} является палиндромом" : $"Число {num} не является палиндром");
